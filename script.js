dbfcd612be6ccffb159e99c4f7eab2af39fe2901
ogzA <akankan.oguz@gmail.com>
2026-07-17 09:52:47 +0200

    feat: add exercise 1

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-//#region Basics-Aufgabe 1 
+//#region Basics-Aufgabe 1
+
+const firstName = "Oğuz";
+let age = 33;
+
+function printName(name, age) {
+	console.log(`Hallo, ich heiße ${name} und bin ${age} Jahre alt.`);
+}
+
+printName(firstName, age);
+
+// #endregion
+
+//#region Basics-Aufgabe 2
 // hier Lösung ergänzen
 // #endregion
 
-//#region Basics-Aufgabe 2 
+//#region Basics-Aufgabe 3
 // hier Lösung ergänzen
 // #endregion
 
-//#region Basics-Aufgabe 3 
-// hier Lösung ergänzen
-// #endregion
-
-//#region Basics-Aufgabe 4 
+//#region Basics-Aufgabe 4
 // hier Lösung ergänzen
 // #endregion
 
