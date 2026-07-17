d03315308f52ff5e5a223097303705ffae88a36e
ogzA <akankan.oguz@gmail.com>
2026-07-17 09:50:40 +0200

    init

---
+++ script.js
@@ -0,0 +1,31 @@
+//#region Basics-Aufgabe 1 
+// hier Lösung ergänzen
+// #endregion
+
+//#region Basics-Aufgabe 2 
+// hier Lösung ergänzen
+// #endregion
+
+//#region Basics-Aufgabe 3 
+// hier Lösung ergänzen
+// #endregion
+
+//#region Basics-Aufgabe 4 
+// hier Lösung ergänzen
+// #endregion
+
+//#region Basics-Aufgabe 5
+// hier Lösung ergänzen
+// #endregion
+
+//#region DOM-Aufgabe 1
+// hier Lösung ergänzen
+// #endregion
+
+//#region DOM-Aufgabe 2
+// hier Lösung ergänzen
+// #endregion
+
+// #region ZUSATZAUFGABE (OPTIONAL!!!)
+// hier Lösung ergänzen
+// #endregion
